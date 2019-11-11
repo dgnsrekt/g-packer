d263850aba08f46802ae01e1a538a6c476109de9
dgnsrekt <dgnsrekt@pm.me>
2019-11-11 10:12:14 -0600

    re orgranized sections.

---
+++ tests/environment.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+TEST_DATA_DIR = ROOT_DIR / "test_data"
+PRE_PROCESS_DATA = TEST_DATA_DIR / "Pre"  # Data pre processing.
+POST_PROCESS_DATA = TEST_DATA_DIR / "Post"  # Data post processing.
+
+TEST_DATA_DIR.mkdir(exist_ok=True)
+PRE_PROCESS_DATA.mkdir(exist_ok=True)
+POST_PROCESS_DATA.mkdir(exist_ok=True)
+
+PACKAGE_DAT_PATH = TEST_DATA_DIR / "package.dat"
+MANIFEST_PATH = TEST_DATA_DIR / "manifest.ini"
