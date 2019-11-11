9e5b43e8bd70fa08efdedbb1904bdda730909c18
dgnsrekt <dgnsrekt@pm.me>
2019-11-11 10:11:59 -0600

    deleted oldfiles.

--- progressplay.py
+++
@@ -1,6 +0,0 @@
-import time
-import progressbar
-
-for i in progressbar.progressbar(range(100), redirect_stdout=True):
-    print("SOME TEST", i)
-    time.sleep(0.02)
--- test_this.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_this():
-    assert True
