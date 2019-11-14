6b87cb30e6011b282158a66f69f846ed2689abba
dgnsrekt <dgnsrekt@pm.me>
2019-11-13 23:11:52 -0600

    refactored example to reflect README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# glitter-packer
+# g-packer
+
 Idea for the bitglitter filepacker.
+
+##Start dev environment.
+
+```
+$ pipenv sync --dev
+$ pipenv shell
+```
+
+##Run test
+After starting the dev environment run.
+```
+$ pytest
+```
+
+##Run test from nox
+After starting the dev environment run.
+```
+$ nox -s tests
+```
+
+##Run Example
+After starting the dev environment run.
+```
+In the example.py change target_pack_folder to the 
+location you would like to pack.
+
+$ python example.py
+```
+
--- example.py
+++ example.py
@@ -6,22 +6,30 @@ from g_packer.static import MEGABYTE
 
 def main():
 
-    target_folder = "stuff"  # FOLDER TO PACK make sure folder or file exists.
+    target_pack_folder = "stuff"  # FOLDER TO PACK make sure folder or file exists.
+    target_unpack_folder = "new_folder"
+
+    target_manifest_location = "manifest.ini"
+    target_package_dat_location = "package.dat"
 
     buffer_size = MEGABYTE * 2
 
-    manifest = ManifestMaker.create(target_folder, buffer_size)  # createst a manifest object
+    manifest = ManifestMaker.create(target_pack_folder, buffer_size)  # createst a manifest object
 
-    ManifestMaker.write(manifest, "manifest.ini", buffer_size)  # writes to a manifest.ini file
+    ManifestMaker.write(
+        manifest, target_manifest_location, buffer_size
+    )  # writes to a manifest.ini file
 
-    PackageMaster.pack(manifest, buffer_size, VersionOnePackageProcessor, "package.dat")
+    PackageMaster.pack(
+        manifest, buffer_size, VersionOnePackageProcessor, target_package_dat_location
+    )
     # ^--- reads then serializes each file in the manifest by BUFFER_SIZE chunks.
     # Uses the VersionOnePackageProcessor to read and serailize each file in the manifest
     # file by BUFFER_SIZE chunks then writes to the data to a package.dat file.
     # PackageProcessor takes care of compress -> metadata collection -> serialization -> writing package.dat
     # The packageProcessor can easily be upgraded to encrypt each chunk prior to compress/ serialization.
 
-    PackageMaster.unpack("package.dat", "new_folder")
+    PackageMaster.unpack("package.dat", target_unpack_folder)
     # Takes care of unpacking a package.dat file and writes to the root new_folder directory.
 
 
