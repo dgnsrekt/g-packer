34509be9ff1130d02537cf4dc48409ffff12deb0
dgnsrekt <dgnsrekt@pm.me>
2019-11-13 19:10:21 -0600

    added docstrings to packer

--- g_packer/packer.py
+++ g_packer/packer.py
@@ -33,7 +33,7 @@ class BasePacker:
 class BaseUnPacker:
     version = None
 
-    def parse_metadata(chunk):
+    def parse_metadata():
         raise NotImplementedError
 
     def decompress():
@@ -45,7 +45,7 @@ class BaseUnPacker:
     def hash():
         raise NotImplementedError
 
-    def check_hash():
+    def compare_hash():
         raise NotImplementedError
 
     def write():
@@ -53,9 +53,17 @@ class BaseUnPacker:
 
 
 class VersionOnePackageProcessor(BasePacker, BaseUnPacker):
+    """
+    Version 1 Package Processor
+    Extends from the Packer/Unpacker base classes and provides functions
+    needed to encrypt, compress, serailize, deserialize pack and unpack 
+    multiple files into a single package.dat file.
+    """
+
     version = 1
 
     def hash(file_chunk):
+        """Returns the first/last bytes of the sha256 hash of the input chunk."""
         hash_algo = sha256()
         file_hash = hash_algo.update(file_chunk)
         file_hash = hash_algo.hexdigest()
@@ -66,6 +74,7 @@ class VersionOnePackageProcessor(BasePacker, BaseUnPacker):
         return first_bytes + last_bytes
 
     def compress(file_chunk):
+        """Returns the compressed version of a file chunk."""
 
         # save_bytes = len(file_chunk) - len(compressed_chunk)  # noqa
         # print("using compressed chunk")  # TODO: add else for logging.
@@ -77,6 +86,7 @@ class VersionOnePackageProcessor(BasePacker, BaseUnPacker):
             return file_chunk, False
 
     def collect_metadata(file_name, chunk_hash, index, seek, compressed_flag):
+        """Collects and returns metadata of the file."""
         meta_data = {
             "filename": file_name,
             "hash": chunk_hash,
@@ -89,15 +99,18 @@ class VersionOnePackageProcessor(BasePacker, BaseUnPacker):
         return meta_data
 
     def serialize(data):
+        """Returns a bson encoded version of the inputted data chunk."""
         return bson.encode(data)
 
     def write_package(serialized_data, destination):
-        # print("writing bytes to file")
+        """Appends a serialized data chunk to the destination file."""
         with open(destination, mode="ab") as write_file:
             return write_file.write(serialized_data)
 
-    ################### Unpacker Section
+    #################### Unpacker Section ####################
+
     def parse_metadata(chunk):
+        """Parses and returns a dictionary of metadata from a chunk."""
         meta_data = {
             "version": chunk["version"],
             "filename": chunk["filename"],
@@ -109,15 +122,18 @@ class VersionOnePackageProcessor(BasePacker, BaseUnPacker):
         return meta_data
 
     def decompress(payload, compressed_flag):
+        """Decompresses payload if the compressed_flag is True"""
         if compressed_flag:
             return zlib.decompress(payload)
         else:
             return payload
 
-    def check_hash(first_hash, second_hash):  # Comparehash
-        assert first_hash == second_hash  # TODO: Proper exception # should return, better naming
+    def compare_hash(chunk_hash, other_hash):  # Comparehash
+        """Returns true if the chunk_hash == the other_hash"""
+        assert chunk_hash == other_hash  # TODO: Proper exception # should return, better naming
 
     def write(cleaned_data, filename, root_folder):
+
         folder = Path(root_folder)
         file_path = folder.joinpath(filename)
         # TODO: Think about passing permissions as metadata
@@ -133,16 +149,19 @@ class PackageMaster:
     versions = {1: VersionOnePackageProcessor}
 
     def split(data):
+        """Splits the payload, version from the metadata."""
         payload = data.pop("payload")
         version = data.pop("version")
         return payload, data, version
 
     def get_package_processor(version):
+        """Returns the proper processer based on the version arg."""
         processor = PackageMaster.versions.get(version, None)
         assert processor is not None  # TODO: Add proper error.
         return processor
 
-    def unpack(target_package: str, destination_root_folder: str):  # TODO: ADD root_destination
+    def unpack(target_package: str, destination_root_folder: str):
+        """Processes and Unpacks a package.dat into the destination folder"""
         with open(target_package, mode="rb") as read_file:
             for deserialized_data in bson.decode_file_iter(read_file):
 
@@ -154,7 +173,7 @@ class PackageMaster:
 
                 chunk_hash = processor.hash(payload)
 
-                processor.check_hash(chunk_hash, meta_data["hash"])
+                processor.compare_hash(chunk_hash, meta_data["hash"])
 
                 processor.write(payload, meta_data["filename"], destination_root_folder)
                 print(".", end="", flush=True)
@@ -162,6 +181,7 @@ class PackageMaster:
     def pack(
         manifest: FileManifest, chunk_buffer_size: int, processor: BasePacker, destination: str
     ):
+        """Process and Packs all manifested files into a package.dat file."""
 
         assert issubclass(
             processor, BasePacker
@@ -170,14 +190,12 @@ class PackageMaster:
 
         manifest.verify()
 
-        seek = 0  # Shows the starting location to start read the file from the package.dat.
+        seek = 0  # For recording the start location of each file.
 
         for current_file in manifest:
 
             file_name = str(current_file.path)
             chunks = current_file.chunk_len(chunk_buffer_size)
-
-            # print("chunks:", chunks)
 
             with open(current_file.path, "rb") as read_file:
                 for index in range(chunks):
@@ -198,5 +216,3 @@ class PackageMaster:
 
                     seek += written_bytes
                     print(".", end="", flush=True)
-
-                    # break  # TODO: DONT FORGET TO REMOVE
