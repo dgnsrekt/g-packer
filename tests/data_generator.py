5850d6b21d6bdd7f8a31ec25c424a70df811427d
dgnsrekt <dgnsrekt@pm.me>
2019-11-14 21:07:52 -0600

    added type hinting/ docstrings.

--- tests/data_generator.py
+++ tests/data_generator.py
@@ -16,21 +16,26 @@ MEGABYTE = KILOBYTE * 1024
 
 @logme.log(name="Fake-File-Generator")
 class FakeFileGenerator:
+    """Generates fakes files and folders for testing."""
+
     fake = Faker()
 
-    def __init__(self, directory):  # think about making this a config env var
+    def __init__(self, directory: str):
         self.directory = Path(directory)
         self.root_directory = self.directory / self.random_folder()
 
     @staticmethod
     def random_folder():
+        """Randomly generates a foldername."""
         return FakeFileGenerator.fake.file_name().split(".")[0]
 
     @staticmethod
     def random_file():
+        """Randomly generates a filename."""
         return FakeFileGenerator.fake.file_name()
 
-    def generate_files(self, *, num_directories, num_files):  # may get rid of self
+    def generate_files(self, *, num_directories: int, num_files: int):
+        """Creates random files, directories, and sub directories."""
         self.root_directory.mkdir()
 
         for _ in range(num_directories):
@@ -38,15 +43,16 @@ class FakeFileGenerator:
             directory.mkdir()
 
             for _ in range(num_files):
-                file_ = directory / self.random_file()
-                self.logger.debug(f"creating file{file_}")
-                file_.touch()
+                new_file = directory / self.random_file()
+                self.logger.debug(f"creating file{new_file}")
+                new_file.touch()
 
-    def generate_data(self, file_size):
+    def generate_data(self, file_size: int):
+        """Generates random bytes into each file."""
         for directory, _, files in walk(self.root_directory):
-            for file_ in files:
-                file_ = Path(directory) / file_
-                self.logger.debug(f"writing to file:\n{file_}")
+            for file_name in files:
+                current_file = Path(directory) / file_name
+                self.logger.debug(f"writing to file:\n{current_file}")
                 for _ in progressbar(range(file_size)):
-                    with open(file_, mode="ab") as write_file:
+                    with open(current_file, mode="ab") as write_file:
                         write_file.write(getrandom(size=MEGABYTE))
